e2c32b0aafa5983cf43f11db9afb4f4508d1a67d
Deepankuri <39939904+Deepankuri@users.noreply.github.com>
2018-06-06 14:36:51 +0530

    Added a1-5.py file

---
+++ a1-5.py
@@ -0,0 +1,14 @@
+'''
+using the placehoders
+'''
+s = "Acadview"
+course = "Python"
+fee = 5000
+print(('%s\n%s\n%d') % (s, course, fee))
+'''
+another example
+'''
+name = "Tony Stark"
+salary = 10000
+print(('%s,%d') % (name, salary))
+
