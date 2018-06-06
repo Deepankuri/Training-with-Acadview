d620a38c16e45e1e8c58359a8f151c01a537f3bc
Deepankuri <39939904+Deepankuri@users.noreply.github.com>
2018-06-06 14:08:57 +0530

    Added assignment1-3.py file

---
+++ assignment1-3.py
@@ -0,0 +1,9 @@
+'''
+A program to take an input of three variables from the user
+'''
+print('You have to enter three variables')
+x = input("Enter first var")
+y = input("Enter second var")
+z = input('Enter third var')
+print('The three variables are')
+print(x, y, z)
